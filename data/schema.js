c96a4cf633bd36fd64a795d860465b14488e530c
Robin Ricard <ricard.robin@gmail.com>
2015-10-12 17:50:26 +0200

    Start to modelize the schema

--- data/schema.js
+++ data/schema.js
@@ -7,15 +7,45 @@ var {
   GraphQLString
 } = require('graphql');
 
+var {
+  nodeDefinitions,
+  fromGlobalId
+} = require('graphql-relay');
+
+var FileType = new GraphQLObjectType({
+  name: "File",
+  description: "Represents a file in the FileSystem",
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      description: "The file's name in the FS",
+      resolve: (file) => "TODO" // TODO
+    }
+  })
+});
+
+var {nodeInterface, nodeField} = nodeDefinitions(
+  (globalId) => {
+    var {type, id} = fromGlobalId(globalId);
+    return "TODO"; // TODO
+  },
+  (obj) => {
+    // TODO: give the possibility to support other types
+    return FileType;
+  }
+);
+
+
 var MobileSyncGraphQLSchema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
+    description: "Read-Only access to the file system",
     fields: {
-      hello: {
-        type: GraphQLString,
-        resolve() {
-          return Promise.resolve('world');
-        }
+      node: nodeField,
+      root: {
+        type: FileType,
+        description: "Gets the root element of the file system",
+        resolve: () => "TODO" // TODO
       }
     }
   })
