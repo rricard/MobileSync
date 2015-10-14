0abf27cc0384502769e2aba1ef2fb1eaa54e8890
Robin Ricard <ricard.robin@gmail.com>
2015-10-14 08:43:25 +0200

    Finish the theorical Relay system
    Doesn't work though because of some fetch-related errors inside of the Realy's networking layer.

--- src/components/App.js
+++ src/components/App.js
@@ -6,45 +6,27 @@ import {
 } from "react";
 
 import {
-  StyleSheet,
-  Text,
-  View
+  Navigator
 } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import {
+  RootContainer
+} from "react-relay";
+
+import FileList from "./FileList.js";
+import DirectoryRoute from "../routes/DirectoryRoute.js";
+import RootRoute from "../routes/RootRoute.js";
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          Common to iOS and Android
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={null}
+        renderScene={(route, navigator) => (
+          <RootContainer
+            Component={FileList}
+            route={route ? new DirectoryRoute(route) : new RootRoute()}/>
+        )}/>
     );
   }
 }
-
-
-
-module.exports = App;
--- src/index.android.js
+++ src/index.android.js
@@ -5,6 +5,7 @@ import {
   AppRegistry
 } from 'react-native';
 
+import _ from "./network.js";
 import App from './components/App.js';
 
 AppRegistry.registerComponent('MobileSync', () => App);
