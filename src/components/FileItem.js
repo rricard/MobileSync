ecffa6fff66e2c0e4678468a9a057e7311865019
Robin Ricard <ricard.robin@gmail.com>
2015-10-21 13:09:42 +0200

    Working example
    A simple list with the root of the fs listed.

--- src/components/FileItem.js
+++ src/components/FileItem.js
@@ -1,2 +1,33 @@
 /* @flow */
 'use strict';
+
+import React, {
+  View,
+  Text,
+  ListView,
+  Component
+} from "react-native";
+
+import Relay from "react-relay";
+
+class FileItem extends Component {
+  render(): Component {
+    const {file} = this.props;
+    return (
+      <View>
+        <Text>{file.name}</Text>
+      </View>
+    );
+  }
+}
+
+export default Relay.createContainer(FileItem, {
+  fragments: {
+    file: () => Relay.QL`
+      fragment on File {
+        id
+        name
+      }
+    `
+  }
+});;
--- src/components/FileList.js
+++ src/components/FileList.js
@@ -9,6 +9,8 @@ import React, {
 
 import Relay from "react-relay";
 
+import FileItem from "./FileItem.js";
+
 type FileListProps = {
   file: any
 };
@@ -16,27 +18,22 @@ type FileListProps = {
 class FileList extends Component {
   constructor(props: FileListProps) {
     super(props);
-    this.state = {
-      dataSource: null
-    };
-  }
-
-  componentWillReceiveProps(props: FileListProps) {
     var ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1.id !== r2.id
     });
-    this.setState({
+    this.state = {
       dataSource: ds.cloneWithRows(props.file.children.edges)
-    });
+    };
   }
 
   render() {
-    return (
+    return this.state && this.state.dataSource ?
       <ListView
         dataSource={this.state.dataSource}
-        renderRow={(rowData) => <Text>{JSON.stringify(rowData)}</Text>}
-    />
-    );
+        renderRow={(rowData) => {
+          return <FileItem file={rowData.node}/>
+        }} /> :
+      <Text>Loading...</Text>;
   }
 }
 
@@ -50,8 +47,7 @@ export default Relay.createContainer(FileList, {
         children(first: 10) {
           edges {
             node {
-              id
-              name
+              ${FileItem.getFragment('file')}
             }
           }
         }
