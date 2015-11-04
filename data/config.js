9b1b6cd23d102c3be42fda752cd6a580bcf896ee
Robin Ricard <ricard.robin@gmail.com>
2015-11-04 15:43:09 +0100

    Serve statically the files on the server
    Use a classic express middleware

---
+++ data/config.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import path from "path";
+
+export const FS_PATH = path.join(
+  __dirname,
+  "..",
+  process.env.FS_PATH || "./data/example"
+);
+
+export const ROOT_NODE: File = {
+  id: "/",
+  name: "Root",
+  isDirectory: true
+};
