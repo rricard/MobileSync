3b93a44249086583ab6edf7103a47df72c23034a
pschupp <paul.schupp01@gmail.com>
2015-11-18 11:14:43 +0100

    Add Cache and Cache test able to fetch and store

---
+++ src/lib/Cache.js
@@ -0,0 +1,25 @@
+/* @flow */
+'use strict';
+
+export default class Cache {
+  cache: {[key: string]: string};
+
+  constructor() {
+    this.cache = {};
+  }
+
+  fetch(id: string, url: string): Promise<string> {
+    return this.cache[id] ?
+      Promise.resolve(this.cache[id]) :
+      this._fetchAndCache(id,url);
+  }
+
+  _fetchAndCache(id: string, url: string): Promise<string> {
+    return fetch(url)
+    .then(res => res.text())
+    .then(text => {
+      this.cache[id] = text;
+      return text;
+    });
+  }
+}
---
+++ src/lib/__tests__/Cache.js
@@ -0,0 +1,30 @@
+jest.dontMock("../Cache.js");
+
+global.fetch = (url) => Promise.resolve({
+  text: () => Promise.resolve(url)
+});
+
+const Cache = require("../Cache.js");
+
+describe('Cache', function() {
+  beforeEach(function() {
+    this.cache = new Cache();
+  });
+  describe('cacheFetch', function() {
+    it('should fetch the url properly for the first time', function(done) {
+      this.cache.fetch("ID","URL1")
+      .then(text => {
+        expect(text).toBe("URL1")
+        done();
+      });
+    });
+    it('should fetch the first url properly for the second time', function(done) {
+      this.cache.fetch("ID","URL1")
+      .then(() => this.cache.fetch("ID","URL2"))
+      .then(text => {
+        expect(text).toBe("URL1")
+        done();
+      });
+    });
+  });
+});
