1c25cff329645fa53db17e4464ff1f25093f128b
Robin Ricard <ricard.robin@gmail.com>
2015-12-14 13:01:53 +0100

    Finish patching all react occurences for react-native

--- relay-patcher/patch.sh
+++ relay-patcher/patch.sh
@@ -7,10 +7,13 @@ cp -f ./node_modules/react-relay/lib/RelayContainer.js ./node_modules/react-rela
 cp -f ./node_modules/react-relay/lib/RelayPropTypes.js ./node_modules/react-relay/lib/RelayPropTypes.js.backup
 cp -f ./node_modules/react-relay/lib/RelayRootContainer.js ./node_modules/react-relay/lib/RelayRootContainer.js.backup
 cp -f ./node_modules/react-relay/lib/RelayRenderer.js ./node_modules/react-relay/lib/RelayRenderer.js.backup
+cp -f ./node_modules/react-static-container/lib/StaticContainer.react.js ./node_modules/react-static-container/lib/StaticContainer.react.js.backup
 rm ./node_modules/react-relay/node_modules/fbjs/lib/fetchWithRetries.js
 rm ./node_modules/react-relay/lib/RelayContainer.js
 rm ./node_modules/react-relay/lib/RelayPropTypes.js
 rm ./node_modules/react-relay/lib/RelayRootContainer.js
+rm ./node_modules/react-relay/lib/RelayRenderer.js
+rm ./node_modules/react-static-container/lib/StaticContainer.react.js
 
 cp -f ./relay-patcher/contents/fbjs-fetch.js.replace ./node_modules/fbjs/lib/fetch.js
 cp -f ./relay-patcher/contents/asap-raw.js.replace ./node_modules/asap/raw.js
@@ -18,5 +21,6 @@ cp -f ./relay-patcher/contents/fbjs-fetch.js.replace ./node_modules/react-relay/
 sed "s/'react'/'react-native'/g" < ./node_modules/react-relay/lib/RelayContainer.js.backup | sed "s/'react-dom'/'react-native'/g" > ./node_modules/react-relay/lib/RelayContainer.js
 sed "s/'react'/'react-native'/g" < ./node_modules/react-relay/lib/RelayPropTypes.js.backup > ./node_modules/react-relay/lib/RelayPropTypes.js
 sed "s/'react'/'react-native'/g" < ./node_modules/react-relay/lib/RelayRootContainer.js.backup > ./node_modules/react-relay/lib/RelayRootContainer.js
+sed "s/'react'/'react-native'/g" < ./node_modules/react-relay/lib/RelayRenderer.js.backup > ./node_modules/react-relay/lib/RelayRenderer.js
+sed "s/'react'/'react-native'/g" < ./node_modules/react-static-container/lib/StaticContainer.react.js.backup > ./node_modules/react-static-container/lib/StaticContainer.react.js
 sed "s/ fetch(/ fetch.fetch(/g" < ./node_modules/react-relay/node_modules/fbjs/lib/fetchWithRetries.js.backup > ./node_modules/react-relay/node_modules/fbjs/lib/fetchWithRetries.js
-sed "s/ fetch(/ fetch.fetch(/g" < ./node_modules/react-relay/lib/RelayRenderer.js.backup > ./node_modules/react-relay/lib/RelayRenderer.js
