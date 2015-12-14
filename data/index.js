d8993eb4e4491949ef49cde5be5ab7a91a5c4bba
pschupp <paul.schupp01@gmail.com>
2015-12-14 09:09:46 +0100

    update RN/Babel/relay

--- data/index.js
+++ data/index.js
@@ -1,13 +1,12 @@
 /* @flow */
 "use strict";
 
-require("babel/register");
+require("babel-register");
 
 // Export the GraphQL Schema at launch
 require("./export-schema.js");
 var app = require('./app.js');
 
 const PORT = process.env.PORT || 8000;
-
 app.listen(PORT);
 console.info("App available on http://localhost:" + PORT)
