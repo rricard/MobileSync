eedcfb93f07f8309f2d7cd0a1ff7fb8a95199dd2
Robin Ricard <ricard.robin@gmail.com>
2015-12-14 12:00:40 +0100

    Compiles on iOS
    should too on Android

--- relay-patcher/patch.sh
+++ relay-patcher/patch.sh
@@ -1,4 +1,6 @@
 echo "Patching relay for react-native ..."
+cp -f ./node_modules/fbjs/lib/fetch.js ./node_modules/fbjs/lib/fetch.js.backup
+cp -f ./node_modules/asap/raw.js ./node_modules/asap/raw.js.backup
 cp -f ./node_modules/react-relay/node_modules/fbjs/lib/fetchWithRetries.js ./node_modules/react-relay/node_modules/fbjs/lib/fetchWithRetries.js.backup
 cp -f ./node_modules/react-relay/node_modules/fbjs/lib/fetch.js ./node_modules/react-relay/node_modules/fbjs/lib/fetch.js.backup
 cp -f ./node_modules/react-relay/lib/RelayContainer.js ./node_modules/react-relay/lib/RelayContainer.js.backup
@@ -9,6 +11,8 @@ rm ./node_modules/react-relay/lib/RelayContainer.js
 rm ./node_modules/react-relay/lib/RelayPropTypes.js
 rm ./node_modules/react-relay/lib/RelayRootContainer.js
 
+cp -f ./relay-patcher/contents/fbjs-fetch.js.replace ./node_modules/fbjs/lib/fetch.js
+cp -f ./relay-patcher/contents/asap-raw.js.replace ./node_modules/asap/raw.js
 cp -f ./relay-patcher/contents/fbjs-fetch.js.replace ./node_modules/react-relay/node_modules/fbjs/lib/fetch.js
 sed "s/'react'/'react-native'/g" < ./node_modules/react-relay/lib/RelayContainer.js.backup | sed "s/'react-dom'/'react-native'/g" > ./node_modules/react-relay/lib/RelayContainer.js
 sed "s/'react'/'react-native'/g" < ./node_modules/react-relay/lib/RelayPropTypes.js.backup > ./node_modules/react-relay/lib/RelayPropTypes.js
